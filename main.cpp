d750d1ec54d7f0f3e837afa559dfd9c83b7509f2
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2017-09-29 17:07:56 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main (int argc, char*argv[])
   if ((isdigit (argv [1][0])) && (isdigit (argv [3][0])))
   {
     switch (operand)
+    {
       case '+':
     cout<< atoi (argv[1]) + atoi (argv[3])<< endl;
       break;
@@ -17,6 +18,7 @@ int main (int argc, char*argv[])
       break;
     default:
     cerr<< "ERROR"<< endl;
-  };
+   };
+  }
   else { cerr<< "ERROR"<< endl;};
 }
